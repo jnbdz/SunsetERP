d34121863da3d41c4330cdc44a4b4b18d2e2ad38
jnbdz <jn@yaloub.com>
2024-05-06 18:56:21 -0400

    Added working version

--- .old/cli/Main.java
+++ .old/cli/Main.java
@@ -1,16 +1,15 @@
 package org.sitenetsoft.sunseterp.cli;
 
 import picocli.CommandLine;
+import io.quarkus.picocli.runtime.annotations.TopCommand;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Main class for the SunsetERP CLI application.
- */
+@TopCommand
 @CommandLine.Command(name = "SunsetERP", mixinStandardHelpOptions = true, version = "1.0",
         description = "SunsetERP Command Line Interface")
-public class Main {
+public class Main implements Runnable {
 
     // Map of subcommands
     private static final Map<String, Class<?>> subcommands = new HashMap<>();
@@ -22,17 +21,24 @@ public class Main {
         subcommands.put("service", ServiceCommand.class);
     }
 
+    @Override
+    public void run() {
+        // Typically you might handle some default action here if no subcommands are specified
+        // For now, it does nothing, allowing Picocli to handle help or version commands
+    }
+
     /**
      * Main method for the CLI application.
      * @param args Command-line arguments.
      */
-    public static void main(String[] args) {
+    public static void init(String[] args) {
         CommandLine commandLine = new CommandLine(new Main());
 
         for (Map.Entry<String, Class<?>> entry : subcommands.entrySet()) {
             commandLine.addSubcommand(entry.getKey(), entry.getValue());
         }
 
-        commandLine.execute(args);
+        int exitCode = commandLine.execute(args);
+        System.exit(exitCode);
     }
 }
--- src/main/java/org/sitenetsoft/sunseterp/Startup.java
+++ src/main/java/org/sitenetsoft/sunseterp/Startup.java
@@ -5,11 +5,24 @@ import jakarta.enterprise.event.Observes;
 import io.quarkus.runtime.StartupEvent;
 import org.sitenetsoft.sunseterp.framework.start.Start;
 
+/**
+ * This class is used to start the application when the Quarkus is starting up.
+ * It is an application scoped bean.
+ */
 @ApplicationScoped
 public class Startup {
 
-    /*void onStart(@Observes StartupEvent ev) {
+    /**
+     * This method is called when the Quarkus is starting up.
+     * It will load the objects (containers (not like Docker containers), services, etc.) and start the application.
+     *
+     * @param ev The event that is fired when the Quarkus is starting up.
+     */
+    void onStart(@Observes StartupEvent ev) {
         String[] args = {}; // You can replace this with actual arguments if needed
+
+        // Originally, the main method of the Start class for OFBiz
         Start.main(args);
-    }*/
+    }
+
 }
